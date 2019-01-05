2da77e243df9bb254a8dce2fbe203449dbf74f16
NaveenChahar <40751814+NaveenChahar@users.noreply.github.com>
2019-01-05 17:54:00 +0530

    Add files via upload

---
+++ models/setterGetter/empSetGet.js
@@ -0,0 +1,45 @@
+const object={
+    id:null,
+    name:null,
+    password:null,
+    address:[{
+        fulladdress:null,  
+        city:null,
+        street:null,  
+        state:null,  
+        pin_code:null,
+    }],
+      
+    email:null,  
+    mobile_no:null,  
+    qualification:null,
+    gender:null,
+    referralCode:null,  
+    date_of_birth:null,  
+    nominee:null,
+    nomineeRel:null,
+    typeEmployee:null, 
+    documents:[{
+        GSTNumber:null,
+        nomineeAdhno:null,
+        adhno:null,  
+        bankacno:null,  
+        pancardno:null, 
+    
+    }], 
+    policeVerificationImg:{
+        data:null,
+        contentType:null
+    },
+    panCardPhoto:{
+        data:null,
+        contentType:null
+    }
+    
+
+
+
+
+}
+
+module.exports=object;
