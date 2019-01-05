1ce1f457c604aaf2bec255b2045f86b0d50dc3b0
NaveenChahar <40751814+NaveenChahar@users.noreply.github.com>
2019-01-05 17:54:37 +0530

    Add files via upload

---
+++ Utils/multer/uploadCode.js
@@ -0,0 +1,81 @@
+const multer=require('multer');
+const path=require('path');
+
+const aws = require('aws-sdk');
+const multerS3 = require('multer-s3');
+const s3 = new aws.S3({});
+
+var files={
+     policeVerification:false,
+     adharCard:false,
+     gST:false,
+     nomineePhoto:false,
+     panCardPhoto:false,
+     mobile_no:null
+
+};
+
+const storage = multer.diskStorage({
+    destination:"./uploads/",
+    filename:function(req,file,cb){
+      files[file.fieldname]=true;
+      files.mobile_no=req.body.mobile_no;
+        cb(null,file.fieldname +' '+req.body.mobile_no+ path.extname(file.originalname));
+
+    }
+})
+function checkFileType(file,cb){
+  const filetypes =/jpeg|png|jpg|gif/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+  if(extname && mimetype){
+      return cb(null,true)
+  }else{
+      return cb('error message');
+  }
+  }
+
+
+  const upload = multer({
+  storage:storage,
+ limits:{fileSize:1000000000},
+  fileFilter:function(res,file,cb){
+      checkFileType(file,cb)
+  }
+}).fields([
+{
+  name:'policeVerification'
+},
+{
+  name:'adharCard'
+},
+{
+  name:'gST'
+},
+{
+  name:'nomineePhoto'
+},{
+  name:'panCardPhoto'
+},
+{
+  name:'cancelCheque'
+}
+]);
+
+/*const storage= multerS3({
+      s3: s3,
+      bucket: 'some-bucket',
+      metadata: function (req, file, cb) {
+        cb(null, {fieldName: file.fieldname +' '+req.body.mobile_no+ path.extname(file.originalname)});
+      },
+      key: function (req, file, cb) {
+        cb(null,file.fieldname +' '+req.body.mobile_no+ path.extname(file.originalname) );
+      }
+    })
+    */
+  
+  
+module.exports={
+  "upload":upload,
+  "files":files
+};
